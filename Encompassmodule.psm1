292185cdb078dbb47a0548a1979e6be2eae5e81c
ndriscol <nathan.d.driscoll@gmail.com>
2016-11-13 11:27:21 +1100

    Add files via upload

---
+++ Encompassmodule.psm1
@@ -0,0 +1,69 @@
+﻿
+
+function Add-ToZipFiles
+{
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+.INPUTS
+   Inputs to this cmdlet (if any)
+.OUTPUTS
+   Output from this cmdlet (if any)
+.NOTES
+   General notes
+.COMPONENT
+   The component this cmdlet belongs to
+.ROLE
+   The role this cmdlet belongs to
+.FUNCTIONALITY
+   The functionality that best describes this cmdlet
+#>
+
+    [CmdletBinding()]
+    [OutputType([String])]
+    Param
+    (
+        
+        [Parameter(Mandatory=$true, 
+                   ValueFromPipeline=$false,
+                   ValueFromPipelineByPropertyName=$false, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=0,
+                   ParameterSetName='Parameter Set 1')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        $DesinationZipFile,
+        [Parameter()]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        $SourceDirectory
+
+    )
+
+   
+
+    try{
+            Add-Type -Assembly System.IO.Compression.FileSystem -ErrorAction Stop
+            $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+            
+        }Catch [System.exception]{
+
+                
+        }
+
+    Try{
+
+            [System.IO.Compression.ZipFile]::CreateFromDirectory($SourceDirectory,$zipfilename, $DesinationZipFile, $false)
+
+        }Catch [System.exception]{
+
+
+        }
+
+}
